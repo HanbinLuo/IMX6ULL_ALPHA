78c4afa47e0a783e7409a2ea1e9e9fcfff6c533e
luohanbin <luohanbin03@outlook.com>
2026-04-04 18:38:50 +0800

    Add device tree and new defconfig

--- imx6ull.sh
+++ imx6ull.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Build the kernel for 正点原子i.MX6ULL V2.4
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_myboard_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
